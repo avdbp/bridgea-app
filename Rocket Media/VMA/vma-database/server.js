b5af7a41ce7edc8b3f9873d8c25df224499e294b
Alejandro van den Bussche <110142819+avdbp@users.noreply.github.com>
2025-09-04 17:06:13 -0400

    Delete Rocket Media/VMA/vma-database directory

--- Rocket Media/VMA/vma-database/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-
-// ℹ️ Sets the PORT for our app to have access to it.
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
